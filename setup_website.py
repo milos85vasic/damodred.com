1cc185b64ead54a8519d8b3ed8fe8d75368e929d
Milos Vasic <milos85vasic@gmail.com>
2018-06-13 19:29:44 +0200

    damodred.com: website setup wizard - work in progress.

--- setup_website.py
+++ setup_website.py
@@ -12,8 +12,11 @@ from os.path import expanduser
 account = sys.argv[1]
 home = expanduser("~")
 
-system_system_configuration = get_system_configuration()
+system_configuration = get_system_configuration()
 
-
+if has_feature(account, feature_mysql):
+    if account in system_configuration:
+        if key_configuration_port_mysql in system_configuration[account]:
+            mysql_port = system_configuration[account][key_configuration_port_mysql]
 
 
